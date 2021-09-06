aaffb397364b6fb2ef9f0518adb280144536bfe2
Azer Masimov <azer.mesimov@starex.az>
2021-09-06 13:18:34 +0400

    first commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,35 @@
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        icon: "sun",
+    },
+    winter: {
+        text: "Burr, its chilly",
+        icon: "snowflake",
+    },
+};
+
+const getSeason = (latitude, month) => {
+    if (month > 2 && month < 9) {
+        return latitude > 0 ? "summer" : "winter";
+    } else {
+        return latitude < 0 ? "winter" : "summer";
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+    const { text, icon } = seasonConfig[season];
+
+    return (
+        <div class={`${season} season-display`}>
+            <i className={`icon-left massive ${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${icon} icon`} />
+        </div>
+    );
+}
+
+export default SeasonDisplay;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  state = { latitude: null, errorMessage: null };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <SeasonDisplay latitude={this.state.latitude} />;
+    }
+
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    return <div>Loading...</div>;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
